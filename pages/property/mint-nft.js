9c30fd8ce5e6472eba62e2311993c6fe6f59c1df
azmat <azmat.rasool@spyresync.com>
2023-01-03 11:37:53 +0500

    mintPropertyNft

--- pages/property/mint-nft.js
+++ pages/property/mint-nft.js
@@ -1,13 +1,31 @@
-import React from 'react'
-import NftsLayout from '../../src/nftsLayout';
+import { Box, Typography,styled } from "@mui/material";
+import React from "react";
+import NftsLayout from "../../src/nftsLayout";
 
+const MintPropertyNfts = styled(Box)(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "space-between",
+  width: "100%",
+  paddingBottom: "80px",
+  borderRight: `0.5px solid ${theme.palette.secondary.darkGray}`,
+}));
 const MintNFTS = () => {
   return (
-    <div>MintNFTS</div>
-  )
-}
+    <>
+      <Box>
+        <Box>
+          <Typography variant="h3">Mint Property NFT</Typography>
+        </Box>
+        <Box>
+
+        </Box>
+      </Box>
+    </>
+  );
+};
 
 export default MintNFTS;
 MintNFTS.getLayout = function (page) {
-    return <NftsLayout>{page}</NftsLayout>;
-  };
+  return <NftsLayout>{page}</NftsLayout>;
+};
