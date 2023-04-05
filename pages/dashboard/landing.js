925220b8a71134beaf10ef43b31f91124443316c
azmat <azmat.rasool@spyresync.com>
2023-04-05 14:49:58 +0500

    youtube video temp

--- pages/dashboard/landing.js
+++ pages/dashboard/landing.js
@@ -8,37 +8,15 @@ import { VIDEO_WATCHED } from "../../src/constants/endpoints";
 
 function Landing() {
   useTitle("Dasboard");
-  const { push, asPath } = useRouter();
+  const { push } = useRouter();
   const [profileInfo, setProfileInfo] = useState({});
-  const [videoCount, setVideoCount] = useState(0);
-  const [player, setPlayer] = useState({});
-
-  useEffect(() => {
-    if (!JSON.parse(localStorage.getItem("videos"))) getVideos();
-  }, []);
   useEffect(() => {
     const profile_info = JSON.parse(localStorage.getItem("profile_info"));
     setProfileInfo(profile_info);
-    if (!profile_info?.user?.isConfirmed) {
+    if (profile_info?.user?.isConfirmed) {
       updateVideoStatus();
-      let updatedData = structuredClone(profile_info);
-      updatedData.user.isConfirmed = true;
-      localStorage.setItem("profile_info", JSON.stringify(updatedData));
     }
-
-    let videoCountLocal = JSON.parse(localStorage.getItem("video_count"));
-    const videosList = JSON.parse(localStorage.getItem("videos"));
-    localStorage.setItem(
-      "video_count",
-      JSON.stringify(
-        videoCountLocal === videosList?.length - 1 ? 0 : videoCountLocal + 1
-      )
-    );
-
-    videoCountLocal === videosList?.length - 1
-      ? setPlayer(videosList?.[0])
-      : setPlayer(videosList?.[videoCountLocal + 1]);
-  }, [asPath]);
+  }, []);
 
   const updateVideoStatus = async () => {
     try {
@@ -49,23 +27,6 @@ function Landing() {
       });
     } catch (error) {
       console.log(error);
-    }
-  };
-
-  const getVideos = async () => {
-    const res = await publicAxios.get(
-      "https://content-youtube.googleapis.com/youtube/v3/playlistItems?playlistId=PL0fdpt9qgI7lPBOwI9GdnZ9leeU5b5v9B&part=snippet&key=AIzaSyBFcsX8Ye6MM42TsUtbEIWnj7sSpmmYwLE&maxResults=50",
-      {}
-    );
-
-    if (res.status == 200) {
-      localStorage.setItem("video_count", JSON.stringify(0));
-      const updatedVideosData = res.data.items.map((item) => ({
-        position: item.snippet.position,
-        resourceId: item.snippet.resourceId,
-      }));
-      setPlayer(updatedVideosData?.[0]);
-      localStorage.setItem("videos", JSON.stringify(updatedVideosData));
     }
   };
 
@@ -80,38 +41,16 @@ function Landing() {
         </Typography>
       </Box>
       <Box>
-        {profileInfo?.user?.isConfirmed ? (
-          <CardMedia
-            component="iframe"
-            src={`https://www.youtube.com/embed/${player?.resourceId?.videoId}`}
-            controls
-            title="title"
-            image={`https://www.youtube.com/embed/${player?.resourceId?.videoId}`}
-            autoPlay
-            allow="autoPlay"
-            sx={{ height: { xs: "150px", md: "373px" }, border: "none" }}
-          />
-        ) : (
-          <CardMedia
-            component="iframe"
-            src="https://consortiamedia.s3.amazonaws.com/Getting+Started+-+WITH+CAPTIONS.mp4"
-            controls
-            title="title"
-            image="https://consortiamedia.s3.amazonaws.com/Getting+Started+-+WITH+CAPTIONS.mp4"
-            autoPlay
-            allow="autoPlay"
-            sx={{ height: { xs: "150px", md: "373px" }, border: "none" }}
-          />
-        )}
-        {/* <iframe
-          width="560"
-          height="315"
-          src={`https://www.youtube.com/embed/${player?.resourceId?.videoId}`}
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen
-        ></iframe> */}
+        <CardMedia
+          component="iframe"
+          src="https://consortiamedia.s3.amazonaws.com/Getting+Started+-+WITH+CAPTIONS.mp4"
+          controls
+          title="title"
+          image="https://consortiamedia.s3.amazonaws.com/Getting+Started+-+WITH+CAPTIONS.mp4"
+          autoPlay
+          allow="autoPlay"
+          sx={{ height: { xs: "150px", md: "373px" }, border: "none" }}
+        />
       </Box>
       <Box
         sx={{
